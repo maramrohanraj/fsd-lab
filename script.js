59f1b4091efe729ac990c63f169118dcc5be2679
Maram Rohan raj <159509015+maramrohanraj@users.noreply.github.com>
2024-07-29 22:22:38 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,54 +1,34 @@
-    let topProgress = document.querySelector('#topProgress');
-    let leftProgress = document.querySelector('#leftProgress');
-    let rightProgress = document.querySelector('#rightProgress');
-    let bottomProgress = document.querySelector('#bottomProgress');
-    let maindiv = document.querySelector('#maindiv');
+const inputValue = document.getElementById("input");
 
-    function start(){
-        let min = parseInt(prompt("Enter the time in only minutes"));
-        if(min<1){
-            alert("Please enter more than one minute");
-            start();
-        }
-        let time = min*60;
-        let totalTime = time;
-
-        let interval = setInterval(() => {
-            let minutes = Math.floor(time/60);
-            let seconds = time%60;
-            maindiv.innerHTML = `${minutes} : ${seconds}`;
-
-            let progress = ((totalTime-time) / totalTime)*100;
-            if(progress<=25){
-                let tpWidth = progress*4;
-                topProgress.style.width = tpWidth + '%';
-                topProgress.innerHTML = tpWidth.toFixed(2) + '%';
-                topProgress.style.backgroundColor = 'green';
-            }
-            else if(progress<=50){
-                let rpHeight = (progress-25)*4;
-                rightProgress.style.height = rpHeight + '%';
-                rightProgress.innerHTML = rpHeight.toFixed(2) + '%';
-                rightProgress.style.backgroundColor = 'green';
-            }
-            else if(progress<=75){
-                let bpWidth = (progress-50)*4;
-                bottomProgress.style.width = bpWidth + '%';
-                bottomProgress.innerHTML = bpWidth.toFixed(2) + '%';
-                bottomProgress.style.backgroundColor = 'green';
-            } 
-            else if(progress<=100){
-                let lpHeight = (progress-75)*4;
-                leftProgress.style.height = lpHeight + '%';
-                leftProgress.innerHTML = lpHeight.toFixed(2) + '%';
-                leftProgress.style.backgroundColor = 'green';
-            }    
-
-            if(time<=0){
-                clearInterval(interval);
-            }
-            time--;
-        },1000);
+document.querySelectorAll(".numbers").forEach(item => {
+  item.addEventListener("click", (e) => {
+    if (inputValue.innerText === "NaN" || inputValue.innerText === "0") {
+      inputValue.innerText = "";
     }
+    inputValue.innerText += e.target.innerHTML.trim();
+  });
+});
 
-    start();
+
+document.querySelectorAll(".operations").forEach(item => {
+  item.addEventListener("click", (e) => {
+    let lastValue = inputValue.innerText.slice(-1);
+    if(e.target.innerHTML === "="){
+        inputValue.innerText = eval(inputValue.innerText);
+    } 
+    else if(e.target.innerHTML === "AC"){
+      inputValue.innerText = "0";
+    }
+    else if(e.target.innerHTML === 'DEL'){
+      inputValue.innerText = inputValue.innerText.slice(0,inputValue.innerText.length-1);
+    }
+    else{
+      if(isNaN(lastValue)){
+        return;
+      }
+      else{
+        inputValue.innerText += e.target.innerHTML;
+      }
+    }
+  });
+});
